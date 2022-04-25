9b5b5c26b25549c02d652161435e0778bf576d05
wrj <846741270@qq.com>
2022-04-25 11:56:47 +0800

    修改了链接格式

--- docs/.vuepress/components/Homepage.vue
+++ docs/.vuepress/components/Homepage.vue
@@ -3,9 +3,6 @@
     <div>
       <h2>首页引导页</h2>
     </div>
-    <div>test
-      <a href="/md/acg/">我喜欢败犬角色的理由 —— 《败犬女主太多了简评》</a>
-    </div>
     <div v-for="(item, index) in page_list" :key="index">
       <h3>{{item[0]}}</h3>
       <div v-for="(item0, index0) in item[1]" :key="index0">
@@ -37,7 +34,7 @@ export default {
       let data_item = []
       for(let path in this.content_data[item]){
         data_item.push(
-            [path, this.content_data[item][path]]
+            [path.slice(0, path.length-1) + ".html", this.content_data[item][path]]
         )
       }
       this.page_list.push([this.page_name[item], data_item])
