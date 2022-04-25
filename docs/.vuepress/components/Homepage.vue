f5bf9f95e1fdb36d8b02db1fd8914fff6e5ba598
wrj <846741270@qq.com>
2022-04-25 11:44:43 +0800

    1123

--- docs/.vuepress/components/Homepage.vue
+++ docs/.vuepress/components/Homepage.vue
@@ -3,6 +3,9 @@
     <div>
       <h2>首页引导页</h2>
     </div>
+    <div>test
+      <a href="/md/acg/article0/">我喜欢败犬角色的理由 —— 《败犬女主太多了简评》</a>
+    </div>
     <div v-for="(item, index) in page_list" :key="index">
       <h3>{{item[0]}}</h3>
       <div v-for="(item0, index0) in item[1]" :key="index0">
